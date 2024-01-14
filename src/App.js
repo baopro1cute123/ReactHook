ec3413e82e44667cc8d4dca8a007b2b0352f5c3b
baopro1cute123 <nguyenbao12c6nh1@gmail.com>
2024-01-14 21:15:15 +0700

    video7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './logo.svg';
+import Nav from './views/Nav';
 function App() {
+  let name = 'Bảo' ;
+  
+  const handleEventClick = (event) => {
+    console.log("click me!", event)
+  }
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{color: 'red', fontSize: '40px'}}>Hello My name is {name}</h1>
+        <input 
+        type='text' 
+        onClick={(event)=> handleEventClick(event)}
+         />
+        <button style={{marginTop: "10px"}} type='button' 
+               onClick={(event)=> handleEventClick(event)}
+        >Click me!</button>
+      
       </header>
     </div>
   );
